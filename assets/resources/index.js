590a7a3b7accde2a75f3ce61d497c42338cb7d8d
Lucien Song <LucienSong@gmx.com>
2022-01-04 01:55:32 +0800

    new version
    new version init

---
+++ assets/resources/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///resources.js",["cc"],(function(){"use strict";var e;return{setters:[function(n){e=n.cclegacy}],execute:function(){e._RF.push({},"0c82eEIO89L9qVCB8mY7x4A","zh_TW",void 0);var n=window;n.languages||(n.languages={}),n.languages.zh_TW={default:{button:"按鍵",back:"返回",ok:"確定",language:"語言"},mains:{nftPresale:"NFT預售",spawingPool:"孵化池",auction:"拍賣行"},login:{info:"需要一個支持幣安智能鏈(BSC)的虛擬貨幣錢包瀏覽器.",connect:"連接錢包"},presale:{nftPresale:"NFT預售",rtPrice:"實時價格",comeSoon:"即將啟動"}},e._RF.pop(),e._RF.push({},"2f0dfqyx8NErLw9Br78oygP","en",void 0);var a=window;a.languages||(a.languages={}),a.languages.en={default:{button:"button",back:"Back",ok:"OK",language:"Language"},mains:{nftPresale:"NFT Pre-Sale",spawingPool:"Spawing Pool",auction:"Auction"},login:{info:"A cryptocurrency wallet that supports Binance Smart Chain(BSC) is required.",connect:"Connect"},presale:{nftPresale:"NFT Pre-Sale",rtPrice:"Real-time Price",comeSoon:"Coming soon"}},e._RF.pop(),e._RF.push({},"4f07fmiZP9JLryBieHM3C9a","zh_CN",void 0);var o=window;o.languages||(o.languages={}),o.languages.zh_CN={default:{button:"按键",back:"返回",ok:"确定",language:"语言"},mains:{nftPresale:"NFT预售",spawingPool:"孵化池",auction:"拍卖行"},login:{info:"需要一个支持币安智能链(BSC)的虚拟货币钱包浏览器.",connect:"连接钱包"},presale:{nftPresale:"NFT预售",rtPrice:"实时价格",comeSoon:"即将开启"}},e._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///resources.js'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
